b32c5e4268b4714309a1a3f2eb613e23168d06b1
Ahmed GRAOUI <151634985+xaviar7@users.noreply.github.com>
2025-08-07 11:29:31 +0100

    Delete watermarker/converter directory

--- watermarker/converter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import watermark, reveal_watermark
-
-urlpatterns = [
-    path('', watermark, name='watermark'),
-    path('reveal/', reveal_watermark, name='reveal_watermark'),
-]
